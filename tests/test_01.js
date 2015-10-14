d2ff242987eceb40d1a649e1e14af0c4eea300ce
russ <team.holmes@gmail.com>
2015-10-14 06:17:08 +0100

    simple test to check sysem works

--- tests/test_01.js
+++ tests/test_01.js
@@ -4,5 +4,15 @@ describe('Array', function() {
       [1,2,3].indexOf(5).should.equal(-1);
       [1,2,3].indexOf(0).should.equal(-1);
     });
+
+    it('should add 2 numbers together', function() {
+      (1 + 6).should.equal(7);
+    });
   });
+
+  describe ('The person class', function() {
+  	it('can be initiated', function() {
+  		var me = new Person();
+  	})
+  })
 });
